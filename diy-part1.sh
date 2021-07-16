74a84c71be4a44c380586eba282247048eb700cf
未日 <33367701+RealKiro@users.noreply.github.com>
2021-07-16 15:37:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,8 +21,9 @@
 # 更改默认主题为第三方主题
 #git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git package/mine/luci-theme-infinityfreedom.git
 #sed -i 's/luci-theme-bootstrap/luci-theme-infinityfreedom/g' package/feeds/luci/luci/Makefile 
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/mine/luci-theme-argon 
-make menuconfig #choose LUCI->Theme->Luci-theme-argon  
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/mine/luci-theme-argon
+./scripts/feeds update -a && ./scripts/feeds install -a
+sed -i 's/luci-theme-bootstrap/luci-theme-argon-dark-mod/g' package/feeds/luci/luci/Makefile
 
 # 复杂的AdGuardHome的openwrt的luci界面
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/mine/luci-app-adguardhome
